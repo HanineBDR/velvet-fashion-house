29798e3a54a6199cc35a6d2d3b0e77ad4c791c84
HanineBDR <hanine.boudour@univ-constantine2.dz>
2025-11-29 16:41:38 +0100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    base:'/velvet-fashion-house/',
     return {
+      base:'/velvet-fashion-house/',
       server: {
         port: 3000,
         host: '0.0.0.0',
